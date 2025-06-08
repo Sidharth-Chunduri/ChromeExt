d2dbe8e71060af8870cb5b2b2a774b2fcc18f8ce
Sidharth-Chunduri <chundurisidharth@gmail.com>
2025-06-08 17:16:03 -0500

    First Commit

---
+++ run.py
@@ -0,0 +1,30 @@
+import subprocess
+from datetime import datetime
+import time
+import random
+import string
+import os
+
+os.chdir("/Users/sidharthchunduri/Scripts/Project_New")
+
+print("Running at", datetime.now())
+
+str_len = random.randint(20, 60)
+out = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(str_len))
+
+num_commits = random.randint(1, 20)
+
+for i in range(num_commits):
+    print(f"{i}th commit")
+    delay = random.randint(0, 60)
+    print(f"Sleeping for {delay} seconds...")
+    time.sleep(delay)
+
+    file_path = "/Users/sidharthchunduri/Scripts/Project_new/daily_log.txt"
+    with open(file_path, "a") as f:
+        f.write(f"{datetime.now()}\n {out}\n")
+
+
+    subprocess.run(["git", "add", file_path])
+    subprocess.run(["git", "commit", "-m", f"{out}"])
+    subprocess.run(["git", "push"])
