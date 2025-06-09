c4022c4c9ad3edee65babd5a9358c7e5c432c7a1
Sidharth-Chunduri <chundurisidharth@gmail.com>
2025-06-09 13:24:57 -0500

    updated random generation

--- run.py
+++ run.py
@@ -9,14 +9,15 @@ os.chdir("/Users/sidharthchunduri/Scripts/Project_New")
 
 print("Running at", datetime.now())
 
-str_len = random.randint(20, 60)
-out = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(str_len))
-
 num_commits = random.randint(1, 20)
 
+print(f"commit and push{num_commits} times")
 for i in range(num_commits):
-    print(f"{i + 1}th commit")
-    delay = random.randint(0, 5)
+    str_len = random.randint(20, 60)
+    out = "".join(random.choice(string.ascii_letters + string.digits) for _ in range(str_len))
+
+    print(f"commit number: {i + 1}")
+    delay = random.randint(0, 3600)
     print(f"Sleeping for {delay} seconds...")
     time.sleep(delay)
 
